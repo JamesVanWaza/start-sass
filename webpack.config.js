cd2e6f8cfcc96c60c74063306cff80a2929916be
JamesVanWaza <jgacuca@gmail.com>
2024-05-13 08:37:20 -0400

    Removed the previous versions

--- webpack.config.js
+++ webpack.config.js
@@ -32,50 +32,6 @@ module.exports = {
     },
     module: {
         rules: [{
-<<<<<<< HEAD
-                test: /\.txt$/,
-                use: 'raw-loader'
-            },
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    // Creates `style` nodes from JS Strings
-                    { loader: 'style-loader' },
-
-                    // Translates CSS into CommonJS
-                    { loader: 'css-loader' },
-
-                    // Compiles Sass to CSS
-                    { loader: 'sass-loader' }
-                ]
-            },
-            // Start here for the URL Loader
-            {
-                test: /\.(png|jpg)$/,
-                use: [
-                    { loader: 'url-loader' }
-                ]
-            },
-            {
-                test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9]\.png|jpg)?$/,
-                use: 'url-loader?limit=10000',
-            },
-            {
-                test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
-                use: 'file-loader',
-            }
-        ]
-    },
-    plugins: [
-        // Home Page
-        new HtmlWebpackPlugin({
-            title: 'Responsive Navigation',
-            filename: 'index.html',
-            template: './src/html-templates/index-template.html'
-        }),
-        new HtmlWebpackPlugin({
-            title: 'OH OH',
-=======
             // Whenever a javascript file is found, babel should run and do not compile node_module files
             test: /\js$/,
             exclude: /(node_modules)/,
@@ -119,14 +75,10 @@ module.exports = {
             logo: './src/images/S-FaviconBackgroundColor@4x.png'
         }),
         new HtmlWebpackPlugin({
->>>>>>> 3d9452091b64715c2dc59869e19d79bdf83498d3
             filename: '404.html',
             template: './src/html-templates/404-template.html'
         }),
         new HtmlWebpackPlugin({
-<<<<<<< HEAD
-            title: 'Sign In',
-=======
             filename: 'auth.html',
             template: './src/html-templates/auth-template.html'
         }),
@@ -135,32 +87,16 @@ module.exports = {
             template: './src/html-templates/index-template.html'
         }),
         new HtmlWebpackPlugin({
->>>>>>> 3d9452091b64715c2dc59869e19d79bdf83498d3
             filename: 'signin.html',
             template: './src/html-templates/signin-template.html'
         }),
         new HtmlWebpackPlugin({
-<<<<<<< HEAD
-            title: 'Signup',
-=======
->>>>>>> 3d9452091b64715c2dc59869e19d79bdf83498d3
             filename: 'signup.html',
             template: './src/html-templates/signup-template.html'
         }),
         new HtmlWebpackPlugin({
-<<<<<<< HEAD
-            title: 'Successful',
             filename: 'successful.html',
             template: './src/html-templates/successful-template.html'
         }),
-        // Favicon
-        new FaviconsWebpackPlugin({
-            logo: './src/images/S-FaviconBackgroundColor@4x.png'
-        })
-=======
-            filename: 'successful.html',
-            template: './src/html-templates/successful-template.html'
-        }),
->>>>>>> 3d9452091b64715c2dc59869e19d79bdf83498d3
     ]
 };
